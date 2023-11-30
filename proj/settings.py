83f5dbcde937d1718cd47b423ab446a559db8c95
yuutotakehara <yuuto50505@gmail.com>
2023-11-30 13:15:14 +0900

    Seconds

--- proj/settings.py
+++ proj/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-78vbnri_1sc5kwd3q*p^h8tf^ll2c2fbg-pc^gt$$ssqq=x#0u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -111,6 +111,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/home/ubuntu/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
